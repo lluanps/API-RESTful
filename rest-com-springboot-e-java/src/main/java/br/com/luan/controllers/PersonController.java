88b67fa593956653d25333b2f944ce714eb88471
Luan Pinheiro <lluanps@gmail.com>
2023-02-19 02:37:12 -0300

    Implementando verbos http

--- rest-com-springboot-e-java/src/main/java/br/com/luan/controllers/PersonController.java
+++ rest-com-springboot-e-java/src/main/java/br/com/luan/controllers/PersonController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,6 +31,26 @@ public class PersonController {
 			produces = MediaType.APPLICATION_JSON_VALUE)
 	public Person findById(@PathVariable(value = "id") String id) throws Exception{
 		return service.findById(id);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST,
+			produces = MediaType.APPLICATION_JSON_VALUE,
+			consumes = MediaType.APPLICATION_JSON_VALUE)
+	public Person create(@RequestBody Person person){
+		return service.create(person);
+	}
+	
+	@RequestMapping(method = RequestMethod.PUT,
+			produces = MediaType.APPLICATION_JSON_VALUE,
+			consumes = MediaType.APPLICATION_JSON_VALUE)
+	public Person update(@RequestBody Person person){
+		return service.update(person);
+	}
+	
+	@RequestMapping(value = "/{id}",
+			method = RequestMethod.DELETE)
+	public void delte(@PathVariable String id) {
+		service.delete(id);
 	}
 
 }
