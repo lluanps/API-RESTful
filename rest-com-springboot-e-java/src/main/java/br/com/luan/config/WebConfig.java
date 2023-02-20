2a3b137cbd9deae20e2ac7a759d4fc6e69f35e35
Luan Pinheiro <lluanps@gmail.com>
2023-02-20 18:43:42 -0300

    Implementando Content Negotiation via HEADER Parameter como padrao

--- rest-com-springboot-e-java/src/main/java/br/com/luan/config/WebConfig.java
+++ rest-com-springboot-e-java/src/main/java/br/com/luan/config/WebConfig.java
@@ -11,12 +11,22 @@ public class WebConfig implements WebMvcConfigurer{
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
 		
 		//Via QUERY PARAM. http://localhost:8080/api/person/v1?mediaType=xml
-		configurer.favorParameter(true)
+		/*
+		   configurer.favorParameter(true)
 			.parameterName("mediaType").ignoreAcceptHeader(true)
 			.useRegisteredExtensionsOnly(false)
 			.defaultContentType(MediaType.APPLICATION_JSON)
 				.mediaType("json", MediaType.APPLICATION_JSON)
 				.mediaType("xml", MediaType.APPLICATION_XML);
+		*/
+		
+		//Via HEADER PARAM. http://localhost:8080/api/person/v1
+		configurer.favorParameter(false)
+		.ignoreAcceptHeader(false)
+		.useRegisteredExtensionsOnly(false)
+		.defaultContentType(MediaType.APPLICATION_JSON)
+			.mediaType("json", MediaType.APPLICATION_JSON)
+			.mediaType("xml", MediaType.APPLICATION_XML);
 	}
 
 }
